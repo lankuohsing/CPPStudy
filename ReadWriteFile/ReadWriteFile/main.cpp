1e5706d076217ce92563b2e8cf94a36b11a80cbc
l00467141 <languoxing@huawei.com>
2018-11-02 22:39:53 +0800

    add ReadWriteFile

---
+++ ReadWriteFile/ReadWriteFile/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>     //包含getline()
+using namespace std;
+int main( void )
+{
+	string s;
+	ofstream outf;
+	ifstream inf;
+	inf.open( "D://///data/in.txt" );          //特别注意，这里是：//  是双斜杠喔~~ 
+									   //打开输出文件
+	outf.open( "D:/data/out.txt" );
+	while (getline( inf, s ))      //getline(inf,s)是逐行读取inf中的文件信息
+	{
+		outf << s << '\n';
+		cout << s << endl;
+	}
+	inf.close();
+
+	system( "pause" );
+	return 0;
+}
