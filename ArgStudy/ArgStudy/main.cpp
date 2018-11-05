b2e01e999b17b37d042f99260836c9b3dc940dd9
l00467141 <languoxing@huawei.com>
2018-11-05 17:58:51 +0800

    add PP

--- ArgStudy/ArgStudy/main.cpp
+++ ArgStudy/ArgStudy/main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 void getString( string * str_p ) {
 	*str_p = "languoxing";
 }
+void getString2( string **str_pp ) {
+	string str = "languoxing";
+	**str_pp = str;
+	cout << **str_pp << endl;
+}
 int main( void )
 {
 	string *str_p = new string();
-	getString( str_p );
+	string **str_pp = &str_p;
+	getString2( str_pp );
 	cout << *str_p << endl;
 	delete str_p;
 	system( "pause" );
